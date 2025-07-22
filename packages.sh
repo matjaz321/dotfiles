092377ffa13f7d4b15051a9f626c20844aa1630a
matjaz321 <matjaz.zavski15@gmail.com>
2025-07-22 22:28:47 +0200

    SNAP PCGKS

--- packages.sh
+++ packages.sh
@@ -15,5 +15,13 @@ for package in "${APT_PACKAGES[@]}"; do
 	sudo apt install -y "$package"
 done
 
+SNAP_PACKAGES=(
+	"tldr"
+)
+
+for package in "${SNAP_PACKAGES[@]}"; do
+	sudo snap install -y "$package"
+done
+
 # According to the docs we have to override the already existing fd that comes preinstalled with ubuntu
 ln -s $(which fdfind) ~/.local/bin/fd
