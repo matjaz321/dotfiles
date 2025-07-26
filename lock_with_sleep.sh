37b167cb55489828e71d211e01ae68b9d2781b52
matjaz321 <matjaz.zavski15@gmail.com>
2025-07-26 21:49:21 +0200

    power saving stuff and more fixes for screen locking

---
+++ lock_with_sleep.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Lock immediately
+i3lock --inside-color=00000000 --ring-color=808080ff --clock --image=/home/matjaz/dotfiles/wallpapers/a_group_of_pink_flowers_blurred.jpg --keyhl-color=ff808080 --bshl-color=ff6666ff --time-color=ffffffff --date-color=ffffffff --insidever-color=ff999980 --ringver-color=ff9999ff --verif-color=ffffffff --noinput-text="No Input" --modif-color=ff999980 --screen=1
+
+(sleep 60 && xset dpms force off) &
